81b334ad8cb29ea6f9293576252af0a3a0daf4e0
Gabriel <Raoni.Gabriel@mastercard.com>
2020-04-22 13:58:11 -0300

    first commit

---
+++ src/main/java/com/github/brunocobalchini/model/Actor.java
@@ -0,0 +1,7 @@
+package com.github.brunocobalchini.model;
+
+public class Actor {
+
+	// TODO: Model this class based on this json:
+	// "id": "nm0001618", "name": "Joaquin Phoenix", "birth_date": "1974-10-28", "participations": [ ]}
+}
