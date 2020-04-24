3715b71c93aecb6d3ae792efebf32dc871cddc21
Gabriel <Raoni.Gabriel@mastercard.com>
2020-04-24 11:26:25 -0300

    implemented REST GET

--- src/main/java/com/github/brunocobalchini/model/Actor.java
+++ src/main/java/com/github/brunocobalchini/model/Actor.java
@@ -1,7 +1,41 @@
 package com.github.brunocobalchini.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Actor {
 
-	// TODO: Model this class based on this json:
-	// "id": "nm0001618", "name": "Joaquin Phoenix", "birth_date": "1974-10-28", "participations": [ ]}
+	private String id;
+	
+	@JsonProperty("name")
+	private String nome;
+
+	@JsonProperty("birth_date")
+	private String birthDate; 
+	
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getBirthDate() {
+		return birthDate;
+	}
+
+	public void setBirthDate(String birthDate) {
+		this.birthDate = birthDate;
+	}
+	
 }
